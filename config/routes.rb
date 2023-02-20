317a8c9db633485cb05e0b354ea87e195d6497c6
鎌田裕也 <yk0417yuya@gmail.com>
2023-02-20 11:32:41 +0900

    add favorite function

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'home/about' => 'homes#about', as: 'about'
-  resources :books, only: %i[index show create edit update destroy]
+  resources :books, only: %i[index show create edit update destroy] do
+    resource :favorites, only: %i[create destroy]
+  end
   resources :users, only: %i[index show edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
